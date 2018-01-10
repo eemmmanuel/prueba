1d0152c3ba71298626ea56b81b52a0e2cb5304a9
EPaat <elipaath@gmail.com>
2018-01-10 17:29:36 -0600

    Crear mi primer programa

---
+++ mayor_de_tres.py
@@ -0,0 +1,8 @@
+
+primer_numero = int (input("primer numero: "))
+segundo_numero = int (input("primer numero: "))
+tercer_numero = int (input("primer numero: "))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print("El numero mas grande es{}".format(resultado))
